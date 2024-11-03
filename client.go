877a75f649b728c39281fc21f668a82089ba015d
DamirPavlik <damir.pavlik@gmail.com>
2024-11-03 13:23:25 +0100

    feat: add reg fn

--- client.go
+++ client.go
@@ -60,3 +60,18 @@ func (c *client) handle(message []byte) {
 		c.err(fmt.Errorf("unknown command %s", cmd))
 	}
 }
+
+func (c *client) reg(args []byte) error {
+	u := bytes.TrimSpace(args)
+	if u[0] != "@" {
+		return fmt.Errorf("skill issues, username must begin with @")
+	}
+
+	if len(u) == 0 {
+		return fmt.Errorf("skill issues, username can not be blank")
+	}
+
+	c.username = string(u) c.register <- c
+
+	return nil
+}
